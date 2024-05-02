99ab150cdbc605d88a4b426b2eb8ec084ef0056e
PRAKTIKAN <PRAKTIKAN@DESKTOP-2G1A4E6>
2024-05-02 11:00:06 +0700

    Add project files.

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_1302223006.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Soraya Haidar Salma", "1302223006", new List<string> { "KPL", "Basis Data" }, 2024),
+            new Mahasiswa("Muhammad Fauzan Majid", "1302220144", new List<string> { "KPL", "Basis Data" }, 2024),
+            new Mahasiswa("Arinza Aurelvia", "1302220022", new List<string> { "KPL", "Basis Data"}, 2024),
+            new Mahasiswa("Muhammada Daffa Fadillah", "1302223124", new List<string> { "KPL", "Basis Data" }, 2024),
+            new Mahasiswa("Muhammad Hashfi Hadyan", "1302220079", new List<string> { "KPL", "Basis Data"}, 2024),
+            new Mahasiswa("Maulana Farras", "1302223333", new List<string> { "KPL", "Basis Data"}, 2024)
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa newvalue)
+        {
+            dataMahasiswa.Add(newvalue);
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        [HttpDelete("{id}")] 
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
+
---
+++ Mahasiswa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace modul10_1302223006
+{    
+    public class Mahasiswa
+    {
+        public string Name { get; set; }
+        public string Nim { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+
+        public Mahasiswa(string Name, string Nim, List<string> Course, int Year)
+        {
+            this.Name = Name;
+            this. Nim = Nim;
+            this. Course = Course;
+            this. Year = Year;
+        }
+    }
+}
+
